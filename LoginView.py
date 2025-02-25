156bcecf191a1197cef9a195f7e02c989be482d4
Sam <119715631+MariMari01@users.noreply.github.com>
2025-02-24 21:01:45 -0800

    Added some method structures

--- LoginView.py
+++ LoginView.py
@@ -1,3 +1,15 @@
+from NotificationPreferences import NotificationPreferences
+
 class LoginView:
     def __init__(self):
         pass
+    def readUsernameTextbox(self):
+        pass
+    def readPasswwordTextbox(self):
+        pass
+    def readEmailTextbox(self):
+        pass
+    def readNotificationPreferences(self, NotifPrefs):
+        NotifObject = NotificationPreferences(NotifPrefs)
+        return NotifObject
+    
